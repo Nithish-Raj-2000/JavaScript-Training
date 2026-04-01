26e230baa6f72babb5e2f37aada7e9fa5b708979
Nithish-Raj-2000 <nithish2000m@gmail.com>
2026-04-01 17:35:08 +0530

    my first push

---
+++ script.js
@@ -0,0 +1,257 @@
+
+// Task 1 👎
+
+// // 1
+// var a = 10;
+// a = 20;
+// var a = 30;
+// console.log(a); // 1.Ans : 30
+
+
+// // 2
+// var a = 5;
+// var a = 15;
+// console.log(a); // 2.Ans : 15
+
+
+// // 3
+// var a = 1;
+// a = 2;
+// a = 3;
+// console.log(a);  // 3.Ans : 3
+
+
+// // 4
+// var a = 7;
+// a = 14;
+// var a = 21;
+// a = 28;
+// console.log(a); // 4.Ans: 28
+
+
+// // 5
+// var a = 100;
+// var a = 200;
+// a = 300;
+// console.log(a); // 5.Ans: 300
+
+
+// // 6
+// var a = 9;
+// console.log(a); // 6.Ans: 9
+
+
+// // 7
+// var a = 11;
+// a = 22;
+// console.log(a); // 7.Ans: 22
+
+
+// // 8
+// var a = 50;
+// var a = 60;
+// var a = 70;
+// console.log(a); // 8.Ans: 70
+
+
+// // 9
+// var a = 2;
+// a = 4;
+// a = 6;
+// var a = 8;
+// console.log(a); // 9.Ans: 8
+
+
+// // 10
+// var a = 99;
+// a = 88;
+// console.log(a); // 10.Ans: 88
+
+
+// // 11
+// let b = 10;
+// b = 20;
+// console.log(b); // 11.Ans:  20
+
+
+// // 12
+// let b = 5;
+// b = 15;
+// b = 25;
+// console.log(b); // 12.Ans:  25
+
+
+// // 13
+// let  b= 1;
+// console.log(b); //Ans: 1
+
+
+// // 14
+// let b = 7;
+// b = 14;
+// console.log(b); //Ans: 14
+
+
+// // 15
+// let b = 100;
+// b = 200;
+// b = 300;
+// console.log(b); //Ans: 300
+
+
+// // 16
+// let b = 9;
+// let b = 18;
+// console.log(b); //Ans: Syntax error 'b' already declared.
+
+
+// // 17
+// let b = 50;
+// b = 60;
+// let b = 70; //(SyntaxError 'b' already declared)
+// console.log(b); //Ans: SyntaxError
+
+
+// // 18
+// let b = 2;
+// b = 4;
+// console.log(b); //Ans: 4
+
+
+// // 19
+// let b = 99;
+// b = 88;
+// b = 77;
+// console.log(b);  //Ans: 77
+
+
+// // 20
+// let b = 11;
+// b = 22;
+// console.log(b); //Ans: 22
+
+
+// // 21
+// const c = 10;
+// console.log(c); //Ans: 10
+
+
+// // 22
+// const c = 5;
+// c = 15; // cannot able to change the constant value. 
+// console.log(c); //Ans: TypeError
+
+
+// // 23
+// const c = 1;
+// const c = 2; //cant't redeclare the variable.
+// console.log(c); //Ana: SyntaxError
+
+
+// // 24
+// const c = 7;
+// console.log(c); //Ana: 7
+
+
+// // 25
+// const c = 100;
+// console.log(c); //Ans: 100
+
+
+// // 26
+// const c = 50;
+// c = 60;
+// console.log(c); //Ana: TypeError
+
+
+// // 27
+// const c = 9;
+// console.log(c); //Ans: 9
+
+
+// // 28
+// const c = 20;
+// const c = 30; //Can't redeclare
+// console.log(c); //Ans: SyntaxError
+
+
+// // 29
+// const c = 99;
+// console.log(c); //Ans: 99
+
+
+// // 30
+// const c = 11;
+// c = 22; //Can't reuse or reassign value.
+// console.log(c); //Ans: TypeError
+
+
+// // 31
+// var a = 1;
+// var a = 2;
+// var a = 3;
+// console.log(a); //Ans: 3
+
+
+// // 32
+// var a = 10;
+// a = 20;
+// a = 30;
+// var a = 40;
+// console.log(a); //Ans: 40
+
+
+// // 33
+// var a = 5;
+// a = 15;
+// console.log(a); //Ans: 15
+
+
+// // 34
+// var a = 100;
+// var a = 200;
+// var a = 300;
+// a = 400;
+// console.log(a); //Ans: 400
+
+
+// // 35
+// var a = 7;
+// console.log(a); //Ans: 7
+
+
+// // 36
+// let b = 3;
+// b = 6;
+// b = 9;
+// console.log(b); //Ans: 9
+
+
+// // 37
+// let b = 12;
+// let b = 24; //can't redeclare same variable.
+// console.log(b); //Ans: SyntaxError
+
+
+// // 38
+// let b = 8;
+// b = 16;
+// console.log(b); //Ans: 16
+
+
+// // 39
+// const c = 2;
+// console.log(c); //Ans: 2
+
+
+// // 40
+// const c = 4;
+// c = 8; //Can't change the value of constant variable.
+// Console.log(c); //Ans: TypeError
+
+
+
+
+
+
+
